bd37c567f454744fcec56cbe9449dbd6116a4acd
soldier <bladerunner13101337@gmail.com>
2026-02-17 11:32:58 +0300

    fix

--- src/components/Widget/WidgetButton.tsx
+++ src/components/Widget/WidgetButton.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Card, CardContent } from "../ui/card";
 import { useNavigate, useSearchParams } from 'react-router';
 
--- src/pages/WidgetPage/WidgetPage.tsx
+++ src/pages/WidgetPage/WidgetPage.tsx
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router';
 import { useStore } from "@/components/zustand/store";
 import { findPlayer, getStats } from "@/utils/requests";
 import { StatsTable } from '@/components/StatsTable/StatsTable';
 const WidgetPage = () => {
-  const { setProfile, profile, setStats, stats } = useStore((store) => store);
+  const { setProfile, profile, setStats} = useStore((store) => store);
   const [searchParams] = useSearchParams();
   const id = searchParams.get("player");
-  const [error, setError] = useState(undefined);
+  const [_, setError] = useState(undefined);
   useEffect(() => {
     if (!id) return;
     if (!profile || profile.id !== id) {
@@ -20,6 +20,7 @@ const WidgetPage = () => {
       getStats(profile.player_id, setStats);
     }
   }, [profile]);
+
   return (
     <div className="dark gap-y-4 text-white border-1 rounded-xl w-100 p-10 bg-background">
       <div className='flex flex-col gap-4'>
